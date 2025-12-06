4406c17d797007beb1d2070d4349fe1d8e3bdbe6
Kristofer Hallin <kristofer@sunet.se>
2025-12-06 16:38:02 +0100

    Only show Scribe stats if partner_id is set.

--- pages/admin.py
+++ pages/admin.py
@@ -1210,11 +1210,11 @@ class Customer:
                             ui.label(
                                 f"Total transcribed minutes: {self.stats.get('total_transcribed_minutes', 0)}"
                             ).classes("text-sm")
-                            ui.label(
-                                f"Transcribed minutes via Sunet Scribe: {self.stats.get('transcribed_minutes', 0)}"
-                            )
 
                             if self.partner_id != "N/A" and self.partner_id != "":
+                                ui.label(
+                                    f"Transcribed minutes via Sunet Scribe: {self.stats.get('transcribed_minutes', 0)}"
+                                )
                                 ui.label(
                                     f"Transcribed minutes via Sunet Play: {self.stats.get('transcribed_minutes_external', 0)}"
                                 ).classes("text-sm")
@@ -1243,8 +1243,8 @@ class Customer:
                             ui.label(
                                 f"Total transcribed minutes: {self.stats.get('total_transcribed_minutes_last_month', 0)}"
                             ).classes("text-sm")
-                            ui.label(f"Transcribed minutes via Sunet Scribe: {self.stats.get('transcribed_minutes_last_month', 0)}").classes("text-sm")
                             if self.partner_id != "N/A" and self.partner_id != "":
+                                ui.label(f"Transcribed minutes via Sunet Scribe: {self.stats.get('transcribed_minutes_last_month', 0)}").classes("text-sm")
                                 ui.label(f"Transcribed minutes via Sunet Play: {self.stats.get('transcribed_minutes_external_last_month', 0)}").classes("text-sm")
 
                 with ui.column().style("flex: 0 0 auto;"):
